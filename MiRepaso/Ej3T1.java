4e48222931a1ab3698051351213eda8d3e258964
esther <72193465+hitos-esther@users.noreply.github.com>
2020-10-25 21:44:56 +0100

    Add files via upload

---
+++ MiRepaso/Ej3T1.java
@@ -0,0 +1,29 @@
+/*
+ * ejrcicio 3 | tema 1
+ * Escribe un programa que muestre por pantalla 10 palabras en inglés junto a su
+ *correspondiente traducción al castellano. Las palabras deben estar distribuidas
+ *en dos columnas y alineadas a la izquierda.
+ * @authora Esther Hitos
+ */
+
+public class Ej3T1 {
+	public static void main(String arg[]){
+		System.out.printf("%-10s| %10s\n", "computer","ordenador");
+		System.out.printf("%-10s| %10s\n", "software license","licencia software");
+		System.out.printf("%-10s| %10s\n", "free software","software libre");
+		System.out.printf("%-10s| %10s\n", "proprietary software","software propietario");
+		System.out.printf("%-10s| %10s\n", "public domain software","software de dominio público");
+		System.out.printf("%-10s| %10s\n", "operating system","sistema operativo");
+		System.out.printf("%-10s| %10s\n", "data modeling","modelado de datos");
+		System.out.printf("%-10s| %10s\n", "collaboratively","colaborativamente");
+		System.out.printf("%-10s| %10s\n", "to share","para compartir");
+		}
+	
+	
+	
+	
+	
+	
+	
+	
+	}
