d52a9f63dc39860061523556a462d4e7c0990edb
esther <72193465+hitos-esther@users.noreply.github.com>
2020-10-28 10:11:27 +0100

    Add files via upload

--- MiRepaso/Ej5T1.java
+++ MiRepaso/Ej5T1.java
@@ -10,15 +10,15 @@
 
 public class Ej5T1{
 	public static void main( String args[]){
+		System.out.println("\033[31m___________________________________________________________");
+		System.out.printf("%-18s %-13s %-18s %-13s %-16s %-16s\n","\033[31m|lunes","\033[32m|martes","\033[33m|miercoles","\033[34m|jueves","\033[35m|viernes","|");
 		System.out.println("___________________________________________________________");
-		System.out.printf("%-10s %-10s %-13s %-10s %-10s %-3s\n","\033[31m|lunes","\033[32m|martes","\033[33m|miercoles","\033[34m|jueves","\033[35m|viernes","|");
-		System.out.println("___________________________________________________________");
-		System.out.printf("%-10s %-10s %-13s %-10s %-10s %-4s\n","|PRO","|SINF","|PRO","|PRO","|SINF","|");
-		System.out.printf("%-10s %-10s %-13s %-10s %-10s %-4s\n","|PRO","|SINF","|PRO","|PRO","|SINF","|");
-		System.out.printf("%-10s %-10s %-13s %-10s %-10s %-4s\n","|BBDD","|SINF","|BBDD","|PRO","|SINF","|");
-		System.out.printf("%-10s %-10s %-13s %-10s %-10s %-3s\n","|BBDD","|PRO","|ED","|BBDD","|FOL","|");
-		System.out.printf("%-10s %-10s %-13s %-10s %-10s %-3s\n","|LM","|LM","|ED","|BBDD","|FOL","|");
-		System.out.printf("%-10s %-10s %-13s %-10s %-10s %-3s\n","|LM","|LM","|ED","|BBDD","|FOL","|");
+		System.out.printf("%-18s %-13s %-18s %-13s %-16s %-16s\n","\033[31m|PRO" ,"\033[34m|SINF","\033[31m|PRO" ,"\033[31m|PRO" ,"\033[34m|SINF","|");
+		System.out.printf("%-18s %-13s %-18s %-13s %-16s %-16s\n","\033[31m|PRO" ,"\033[34m|SINF","\033[31m|PRO" ,"\033[31m|PRO" ,"\033[34m|SINF","|");
+		System.out.printf("%-18s %-13s %-18s %-13s %-16s %-16s\n","\033[32m|BBDD","\033[34m|SINF","\033[32m|BBDD","\033[31m|PRO" ,"\033[34m|SINF","|");
+		System.out.printf("%-18s %-13s %-18s %-13s %-16s %-16s\n","\033[32m|BBDD","\033[31m|PRO" ,"\033[35m|ED"  ,"\033[32m|BBDD","\033[36m|FOL" ,"|");
+		System.out.printf("%-18s %-13s %-18s %-13s %-16s %-16s\n","\033[33m|LM"  ,"\033[33m|LM"  ,"\033[35m|ED"  ,"\033[32m|BBDD","\033[36m|FOL" ,"|");
+		System.out.printf("%-18s %-13s %-18s %-13s %-16s %-16s\n","\033[33m|LM"  ,"\033[33m|LM"  ,"\033[35m|ED"  ,"\033[32m|BBDD","\033[36m|FOL" ,"|");
 	}
 	
 	
