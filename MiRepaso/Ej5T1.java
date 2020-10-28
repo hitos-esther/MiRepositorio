95d8656dab60cf76b313d171ee80159cec1dbaca
esther <72193465+hitos-esther@users.noreply.github.com>
2020-10-28 09:14:43 +0100

    Add files via upload

---
+++ MiRepaso/Ej5T1.java
@@ -0,0 +1,26 @@
+/*
+*ejercicio 5 | tema 1
+*
+*Modifica el programa anterior añadiendo colores. Puedes mostrar cada asigna-
+*tura de un color diferente.
+*
+*@authora Esther Hitos
+*
+*/
+
+public class Ej5T1{
+	public static void main( String args[]){
+		System.out.println("___________________________________________________________");
+		System.out.printf("%-10s %-10s %-13s %-10s %-10s %-3s\n","\033[31m|lunes","\033[32m|martes","\033[33m|miercoles","\033[34m|jueves","\033[35m|viernes","|");
+		System.out.println("___________________________________________________________");
+		System.out.printf("%-10s %-10s %-13s %-10s %-10s %-4s\n","|PRO","|SINF","|PRO","|PRO","|SINF","|");
+		System.out.printf("%-10s %-10s %-13s %-10s %-10s %-4s\n","|PRO","|SINF","|PRO","|PRO","|SINF","|");
+		System.out.printf("%-10s %-10s %-13s %-10s %-10s %-4s\n","|BBDD","|SINF","|BBDD","|PRO","|SINF","|");
+		System.out.printf("%-10s %-10s %-13s %-10s %-10s %-3s\n","|BBDD","|PRO","|ED","|BBDD","|FOL","|");
+		System.out.printf("%-10s %-10s %-13s %-10s %-10s %-3s\n","|LM","|LM","|ED","|BBDD","|FOL","|");
+		System.out.printf("%-10s %-10s %-13s %-10s %-10s %-3s\n","|LM","|LM","|ED","|BBDD","|FOL","|");
+	}
+	
+	
+}
+
