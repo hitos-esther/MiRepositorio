61ef320d81a6d2066311650f990a0085fb57ded6
esther <72193465+hitos-esther@users.noreply.github.com>
2020-10-28 09:13:45 +0100

    Add files via upload

---
+++ JAVA/tema5/Ejemplos clase/EjemploBucle10.java
@@ -0,0 +1,20 @@
+ /*ejrcicio 2 | ejercicios cortos bucles
+ * 
+ * @authora Esther Hitos
+ */
+ 
+public class EjemploBucle10{
+	public static void main ( String args[] ){
+	  
+	  
+		for( int j=7; j>=1; j--){
+			for( int i=1;i<=j;i++){
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	
+	
+	}
+ 
+}
