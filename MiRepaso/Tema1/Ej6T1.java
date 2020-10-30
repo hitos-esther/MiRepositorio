e682a6c714c355b82c29408aef0bb1130b0d7a6c
esther <72193465+hitos-esther@users.noreply.github.com>
2020-10-30 17:39:09 +0100

    Add files via upload

---
+++ MiRepaso/Tema1/Ej6T1.java
@@ -0,0 +1,18 @@
+/*
+ * ejercicio 6 | tema1
+ * 
+ * Escribe un programa que pinte por pantalla una pirámide rellena a base de
+ * asteriscos. La base de la pirámide debe estar formada por 9 asteriscos.
+ * 
+ * @authora Esther hitos garcia
+ */
+
+public class Ej6T1{
+		public static void main(String args[]){
+		System.out.println(     "*");
+		System.out.println(    "***");
+		System.out.println(   "*****");
+		System.out.println(  "*******");	
+		System.out.println( "*********");	
+		}
+}
---
+++ MiRepaso/Tema1/Ej7T1.java
@@ -0,0 +1,24 @@
+/*
+ * ejercicio 7 | tema 1
+ * 
+ * Igual que el programa anterior, 
+ * pero esta vez la pirámide estará hueca 
+ * (se debe ver únicamente el contorno 
+ *  hecho con asteriscos).
+ * 
+ * 
+ * 
+ * @authora Esther Hitos Garcia
+ */
+
+public class Ej7T1{
+	public static void main(String args[]){
+	System.out.println("\033[31m");	//pinta los asteriscos de rojo
+	
+	System.out.println(	   "*");
+	System.out.println(   "* *");
+	System.out.println(	 "*   *");
+	System.out.println(	"*     *");
+	System.out.println("*********");
+	}
+}
