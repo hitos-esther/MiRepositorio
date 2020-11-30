94640bfc1f9c1a0f11fcecd32401e6faf79346c7
esther <72193465+hitos-esther@users.noreply.github.com>
2020-11-30 08:19:13 +0100

    Add files via upload

---
+++ MiRepaso/Tema4/ej17T4.java
@@ -0,0 +1,20 @@
+/*
+ * ej17T4.java
+ * 
+ * Escribe un programa que diga cuál es la última cifra de un número entero 
+ * introducido por teclado.
+ * 
+ * @authora Esther Hitos Garcia
+ */
+
+
+public class ej17T4 {
+	
+	public static void main (String[] args) {
+		
+		System.out.println("Introduce un numero sin decimales superior al 10 plis");
+		int numfinal = Integer.parseInt(System.console().readLine());
+		
+	}
+}
+
